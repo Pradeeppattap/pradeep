d6f22f70d8448a5ad0b3bdbed7e3bb1cab3291ba
Pradeeppattap <132896892+Pradeeppattap@users.noreply.github.com>
2023-05-08 18:28:07 +0530

    Test

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.printf("hello");
+         System.out.printf("hello");
+         System.out.printf("hello");
     }
 }
